304cda8243b5bbfa7247f11e612c1fcc2a9be6e1
uaman@gmail.com <git100500>
2019-10-06 22:19:47 +0300

    add dataset

---
+++ dataset.js
@@ -0,0 +1,14 @@
+export const sprintHistory = [
+    { planned: 40, actual: 48, previous: null, daysInSprint: 10, teamSize: 6, feTeamSize: 2, beTeamSize: 4, teamVelocity: 50, success: true },
+
+    { planned: 36, actual: 43, previous: 48, daysInSprint: 9, teamSize: 6, feTeamSize: 2, beTeamSize: 4, teamVelocity: 43, success: true },
+    { planned: 44, actual: 24, previous: 43, daysInSprint: 9, teamSize: 4, feTeamSize: 1, beTeamSize: 3, teamVelocity: 36, success: false },
+    { planned: 29, actual: 40, previous: 24, daysInSprint: 10, teamSize: 3, feTeamSize: 1, beTeamSize: 2, teamVelocity: 24, success: true },
+    { planned: 22, actual: 11, previous: 40, daysInSprint: 10, teamSize: 3, feTeamSize: 1, beTeamSize: 2, teamVelocity: 23, success: false },
+    { planned: 41, actual: 30, previous: 11, daysInSprint: 10, teamSize: 7, feTeamSize: 3, beTeamSize: 4, teamVelocity: 53, success: true },
+    { planned: 35, actual: 45, previous: 30, daysInSprint: 9, teamSize: 7, feTeamSize: 3, beTeamSize: 4, teamVelocity: 50, success: true },
+    { planned: 36, actual: 27, previous: 45, daysInSprint: 10, teamSize: 6, feTeamSize: 2, beTeamSize: 4, teamVelocity: 40, success: false },
+    { planned: 26, actual: 13, previous: 27, daysInSprint: 10, teamSize: 5, feTeamSize: 2, beTeamSize: 3, teamVelocity: 40, success: false },
+
+    { planned: 26, actual: null, previous: 13, daysInSprint: , teamSize: 5, feTeamSize: 2, beTeamSize: 3, teamVelocity: 50, success: false },
+]
